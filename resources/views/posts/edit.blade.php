375fd74705e645e96da5a30276f046ece8d3c37a
Mami <amistade.s-r-k.000-0039@hotmail.co.jp>
2022-02-19 15:58:21 +0900

    ビューファイル準備

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app_original')
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+      <div class="col-md-8">
+          <form action="#" method="POST">
+              <div class="form-group">
+                  <label>タイトル</label>
+                  <input type="text" class="form-control" value="" name="title">
+              </div>
+              <div class="form-group">
+                  <label>内容</label>
+                  <textarea class="form-control" rows="5" name="body"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">更新する</button>
+          </form>
+      </div>
+  </div>
+</div>
+ @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/create', function () {
+    return view('posts.create');
+});
+
+Route::get('/edit', function () {
+    return view('posts.edit');
+});
+
+Route::get('/index', function () {
+    return view('posts.index');
+});
+
+Route::get('/show', function () {
+    return view('posts.show');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
